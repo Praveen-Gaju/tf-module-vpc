b648273988de80db6ee682081b768384c3f08a44
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-26 23:02:23 +0530

    updating tf-module-vpc directory

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept   = true
+  tags       = merge(
+    var.tags,
+    { Name = "${var.env}-peering" }
+  )
 }
 
 ##public subnets
