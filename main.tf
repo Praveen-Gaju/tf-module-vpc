74b278dc53d00fb36acf29f4e134f2e1e4c23c0c
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-26 22:25:32 +0530

    updating tf-module-vpc directory

--- main.tf
+++ main.tf
@@ -48,10 +48,6 @@ resource "aws_nat_gateway" "nat-gateways" {
   )
 }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
-}
 
 ##Public route table
 resource "aws_route_table" "public-route-table" {
